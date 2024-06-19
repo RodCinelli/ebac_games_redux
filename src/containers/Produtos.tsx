6bfae509f88530661fec0281bdb278b805a9984a
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-06-19 02:22:47 -0300

    Utilizando o Redux Toolkit Query

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,17 +1,30 @@
+import React, { useState, useEffect } from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Game } from '../App'
 import Produto from '../components/Produto'
-
+import { useGetJogosQuery } from '../services/api'
 import * as S from './styles'
 
-type Props = {
-  jogos: Game[]
-}
+const Produtos = () => {
+  const { data: jogos, isLoading } = useGetJogosQuery()
+  const [loading, setLoading] = useState(true)
 
-const Produtos = ({ jogos }: Props) => {
+  useEffect(() => {
+    if (!isLoading) {
+      const timer = setTimeout(() => {
+        setLoading(false)
+      }, 1000) // Define o tempo mínimo de carregamento para 1 segundo (1000 ms)
+
+      return () => clearTimeout(timer)
+    }
+  }, [isLoading])
+
+  if (loading) return <h2>Carregando...</h2>
+
   return (
     <>
       <S.Produtos>
-        {jogos.map((game) => (
+        {jogos?.map((game) => (
           <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
